57a72dbe42c1e381fa89e0ee00bae6e056a09e84
Georgios Koloventzos <g.koloventzos@gmail.com>
2015-06-25 06:59:30 -0400

    tie file with array

--- inlog.pl
+++ inlog.pl
@@ -5,16 +5,18 @@
 #
 use strict;
 use Switch;
+use Cwd;
 use File::Basename;
 use Getopt::Long;
 use File::Find;
+use Tie::File;
 
 my @c_exts = qw(.c .cc .cpp .c++);
 my @java_exts = qw(.java);
-my ($dir, $lang, $message);
+my ($lang, $message) = "";
+my $dir = getcwd;
 my $help;
 my $file = "/etc/inslog.dat"; #file for log
-
 
 sub print_help {
     my $string = <<"MES";
@@ -30,17 +32,27 @@ MES
 }
 
 sub add_java {
+  my ($filename, $message) = @_;
+  tie my @lines, 'Tie::File', $filename or return 1;
+  for (@lines) {
+    print "$_\n";
+  }
+  untie @lines;
+  return 0;
 }
 
 sub eachFile {
   my $fn = $_;
+  return 0 if ($fn eq ".");
   my $fullpath = $File::Find::name;
   #remember that File::Find changes your CWD, 
   #so you can call open with just $_
-  my ($ddir, $filename, $ext) = fileparse($fn);
+  print "$fn, $fullpath\n";
+  my ($filename, $ddir, $ext) = fileparse($fullpath, qr/\.[^.]*/);
+  print "$ddir, $filename, $ext\n";
   switch ($lang) {
     case "java" {
-      if ($ext == ".java"){
+      if ($ext eq ".java"){
         add_java($fn,$message);
       }
     }
@@ -51,14 +63,13 @@ sub eachFile {
   }
 }
 
-
-
 GetOptions ("language=s" => \$lang,
             "help"  => \$help,
             "directory=s" => \$dir,
             "message=s" => \$message,);
 
 print_help() if ($help);
+print "No language specified\n" and exit 0 if ($lang eq "");
 
 find (\&eachFile, $dir);
 
